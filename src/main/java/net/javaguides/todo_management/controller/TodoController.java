d9cba774db916c3dabf921a87a325d25a45ca604
Zongcheng Wang <zongchew@uci.edu>
2024-12-01 00:24:31 -0500

    109. Build Get Todo REST API（"Todo not found with id:3",）

--- src/main/java/net/javaguides/todo_management/controller/TodoController.java
+++ src/main/java/net/javaguides/todo_management/controller/TodoController.java
@@ -2,17 +2,29 @@ package net.javaguides.todo_management.controller;
 
 import lombok.AllArgsConstructor;
 import net.javaguides.todo_management.dto.TodoDto;
+import net.javaguides.todo_management.exception.ResourceNotFoundException;
 import net.javaguides.todo_management.service.TodoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/todos")
 @AllArgsConstructor
 public class TodoController {
     private TodoService todoService;
-
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
     //Build Add Todo REST API
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto) {
