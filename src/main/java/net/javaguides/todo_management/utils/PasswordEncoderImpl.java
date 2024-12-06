c0441c0f442766a54e0dfe229fc0b6cdede7b10e
Zongcheng Wang <zongchew@uci.edu>
2024-12-06 17:44:36 -0500

    160. Connect React App to Register REST API

--- src/main/java/net/javaguides/todo_management/utils/PasswordEncoderImpl.java
+++ src/main/java/net/javaguides/todo_management/utils/PasswordEncoderImpl.java
@@ -8,5 +8,9 @@ public class PasswordEncoderImpl {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         System.out.println(passwordEncoder.encode("admin"));
         System.out.println(passwordEncoder.encode("pass"));
+        String hashedPassword = passwordEncoder.encode("zzzz4");
+        System.out.println(hashedPassword);
+        System.out.println("$2a$10$PmVM/9JN1xdyW/HRpYPq0OTISl.xGh7agF.HaZoiBMeftnnNUFUFy");
+        System.out.println(passwordEncoder.matches("zzzz4",hashedPassword));
     }
 }
