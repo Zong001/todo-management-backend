41a8ecba9893ef3692d3bf3a102ef7ed2972a5f8
Zongcheng Wang <zongchew@uci.edu>
2024-12-05 23:28:45 -0500

    154. Exception Handling in Register REST API

---
+++ src/main/java/net/javaguides/todo_management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package net.javaguides.todo_management.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler  {
+    @ExceptionHandler(TodoAPIException.class)
+    public ResponseEntity<ErrorDetails> handleTodoAPIException (TodoAPIException exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(true)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+}
